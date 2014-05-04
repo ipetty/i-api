696e383fa3199e272bea17b076d9153ef25e009d
luocanfeng <luocanfeng@gmail.com>
2014-05-04 13:39:15 +0800

    提交i-api

---
+++ src/main/java/net/ipetty/vo/UserFormVO.java
@@ -0,0 +1,22 @@
+package net.ipetty.vo;
+
+/**
+ * 用户注册/个人信息修改VO
+ * 
+ * @author luocanfeng
+ * @date 2014年5月4日
+ */
+public class UserFormVO {
+
+	private Integer id;
+	private int uid; // uid
+	private String account; // 爱宠号
+	private String email; // 邮箱地址
+	private String nickname; // 昵称
+	private String avatar; // 头像
+	private String background; // 背景图片
+	private String gender; // 性别
+	private String stateAndRegion; // 地区
+	private String signature; // 个性签名
+
+}
---
+++ src/main/java/net/ipetty/vo/UserVO.java
@@ -0,0 +1,21 @@
+package net.ipetty.vo;
+
+/**
+ * 用户信息VO
+ * 
+ * @author luocanfeng
+ * @date 2014年5月4日
+ */
+public class UserVO {
+
+	private Integer id;
+	private int uid; // uid
+	private String account; // 爱宠号
+	private String email; // 邮箱地址
+	private String nickname; // 昵称
+	private String avatar; // 头像
+	private String gender; // 性别
+	private String stateAndRegion; // 地区
+	private String signature; // 个性签名
+
+}
