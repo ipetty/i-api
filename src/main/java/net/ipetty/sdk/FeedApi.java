8a5db38e07ea28e66795f649392e1c7b87655274
luocanfeng <luocanfeng@gmail.com>
2014-05-13 13:29:20 +0800

    完成首页、广场消息列表功能

--- src/main/java/net/ipetty/sdk/FeedApi.java
+++ src/main/java/net/ipetty/sdk/FeedApi.java
@@ -1,5 +1,8 @@
 package net.ipetty.sdk;
 
+import java.util.Date;
+import java.util.List;
+
 import net.ipetty.vo.FeedFormVO;
 import net.ipetty.vo.FeedVO;
 
@@ -15,5 +18,26 @@ public interface FeedApi {
 	 * 发布消息
 	 */
 	public FeedVO publish(FeedFormVO feed);
+
+	/**
+	 * 根据ID获取消息
+	 */
+	public FeedVO getById(Long id);
+
+	/**
+	 * 根据时间线分页获取消息（广场）
+	 * 
+	 * @param pageNumber
+	 *            分页页码，从0开始
+	 */
+	public List<FeedVO> listByTimelineForSquare(Date timeline, int pageNumber, int pageSize);
+
+	/**
+	 * 根据时间线分页获取消息（我和我关注人的）
+	 * 
+	 * @param pageNumber
+	 *            分页页码，从0开始
+	 */
+	public List<FeedVO> listByTimelineForHomePage(Date timeline, int pageNumber, int pageSize);
 
 }
